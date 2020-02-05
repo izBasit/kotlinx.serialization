682d20b797895496c60da75440671c3d334d2e85
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2020-02-05 17:36:52 +0300

    Change SerialDescriptorForNullable.serialName to comply Kotlin notation

--- runtime/commonMain/src/kotlinx/serialization/internal/NullableSerializer.kt
+++ runtime/commonMain/src/kotlinx/serialization/internal/NullableSerializer.kt
@@ -59,7 +59,7 @@ public class NullableSerializer<T : Any>(private val serializer: KSerializer<T>)
     }
 
     private class SerialDescriptorForNullable(private val original: SerialDescriptor): SerialDescriptor by original {
-        override val serialName: String = original.serialName + ".nullable"
+        override val serialName: String = original.serialName + "?"
         override val isNullable: Boolean
             get() = true
 
